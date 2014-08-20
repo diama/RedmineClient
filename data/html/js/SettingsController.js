88d02f77b95f9d7ba908740fe8071a27e8212d61
Damir Bedretdinov <bedretdinov.dg@molodost.bz>
2014-08-20 17:29:01 +0400

    krclient init

---
+++ data/html/js/SettingsController.js
@@ -0,0 +1,3 @@
+var SettingsController = function($scope){
+
+};
---
+++ data/html/js/TaskController.js
@@ -0,0 +1,46 @@
+/**t
+ * Created by damir on 15.08.14.
+ */
+
+var taskScope;
+
+addon.port.on('taskList', function (message) {
+    taskScope.tasks = message;
+
+    if(message.length == 0) {
+        taskScope.listStatus.statusText = 'Нет данных для отображения';
+        taskScope.currentTask = null;
+    } else {
+        taskScope.listStatus.status = 'hide';
+    }
+
+    taskScope.$apply();
+});
+
+var TasksController = function ($scope) {
+    $scope.tasks = [];
+    $scope.currentTask = null;
+    $scope.listStatus = {
+        status: 'progress',
+        statusText: 'Инициализация'
+    };
+
+    taskScope = $scope;
+
+    $scope.setCurrentTask = function (id) {
+        $scope.currentTask = $scope.tasks[id];
+        taskTimer.startTimer();
+    };
+
+    $scope.resetCurrentTask = function() {
+        taskTimer.resetTimer();
+        $scope.currentTask = null;
+    }
+
+    $scope.showWaitMessage = function(){
+        if ($scope.listStatus.status == 'hide')  {
+            return false;
+        }
+        return true;
+    }
+}
